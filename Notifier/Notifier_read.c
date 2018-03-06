6fbf0247d28d36436baf2777f6969b171ba8d227
sriramhv86 <sriramhv86@gmail.com>
2018-03-06 16:56:17 +0000

    Updated Notifier codes

--- Notifier/Notifier_read.c
+++ Notifier/Notifier_read.c
@@ -16,41 +16,46 @@ int main(int argc, char**argv)
 	
 	fd = open("Notifier", O_CREAT|O_RDONLY, S_IRWXU);
         printf("%d\n", fd);
-        if(fd=-1){
-	        fprintf(stderr, "Error Number %d\n", errno);
+        if(fd!=0){
+	       // fprintf(stderr, "Error Number %d\n", errno);
         	perror("Program");
         }
 
 
 	mqd_t rtos;
-    	rtos = mq_open (MQNAME, O_RDWR);
-    	if (rtos < 0) {
-        	printf ("Error %d (%s) on mq_open.\n",
-        	errno, strerror (errno));
-        	exit (1);
-    	}
 
-    	printf ("Opened mqd_t of %d.\n", rtos);
-	printf ("Waiting to Receiving on mqd_t %d.\n", rtos);
+//	while(1){
+    		rtos = mq_open (MQNAME, O_RDWR);
+    		if (rtos < 0) {
+        		printf ("Error %d (%s) on mq_open.\n",
+        		errno, strerror (errno));
+        		exit (1);
+    		}	
 
-    	if(mq_receive (rtos, buffer, sizeof (buffer), NULL)) 
-		 ret = system("telegram-send --stdin < test \n");
+    		printf ("Opened mqd_t of %d.\n", rtos);
+		printf ("Waiting to Receiving on mqd_t %d.\n", rtos);
 
-    	else{
-        	printf ("Error %d (%s) on mq_receive.\n",
-        	errno, strerror (errno));
-        	exit (1);
-    	}
-    	//printf ("Received [%s].\n", buffer);
+    		if(mq_receive (rtos, buffer, sizeof (buffer), NULL)){ 
+			ret = system("telegram-send --stdin < Notifier \n");
+			printf("Notification sent via telegram\n");
+		}
 
-  	rc = mq_send (rtos, MQMESG, sizeof (MQMESG), 1);
-    	if (rc < 0) {
-        	printf ("Error %d (%s) on mq_send.\n",
-        	errno, strerror (errno));
-        	exit (1);
-    	}
+    		else{
+        		printf ("Error %d (%s) on mq_receive.\n",
+        		errno, strerror (errno));
+        		exit (1);
+    		}
+    		//printf ("Received [%s].\n", buffer);
 
-    	printf("Send successful\n");
+  		rc = mq_send (rtos, MQMESG, sizeof (MQMESG), 1);
+    		if (rc < 0) {
+        		printf ("Error %d (%s) on mq_send.\n",
+        		errno, strerror (errno));
+        		exit (1);
+    		}
+
+    		printf("Ack sent successfully\n");
+//	}
 
 
    	return 0;
--- Notifier/Notifier_write.c
+++ Notifier/Notifier_write.c
@@ -13,45 +13,46 @@ int main(int argc, char**argv)
 	mqd_t rtos;
         int rc;
         char buffer[2048];
+	
+//	while(1){
+        	printf ("Sending message.\n");
 
-        printf ("Sending message.\n");
-
-        rtos = mq_open (MQNAME, O_RDWR);
-        if (rtos < 0) {
-        	printf ("Error %d (%s) on mq_open.\n",
-        	errno, strerror (errno));
-        	exit (1);
-        }
-
-        printf ("Opened mqd_t of %d.\n", rtos);
-
-
-	//if(##write your condition){
-        	rc = mq_send (rtos, MQMESG, sizeof (MQMESG), 1);
-        	if (rc < 0) {
-        		printf ("Error %d (%s) on mq_send.\n",
+        	rtos = mq_open (MQNAME, O_RDWR);
+        	if (rtos < 0) {
+        		printf ("Error %d (%s) on mq_open.\n",
         		errno, strerror (errno));
         		exit (1);
         	}
 
+        	printf ("Opened mqd_t of %d.\n", rtos);
+
+
+		//if(##write your condition){
+        		rc = mq_send (rtos, MQMESG, sizeof (MQMESG), 1);
+        		if (rc < 0) {
+        			printf ("Error %d (%s) on mq_send.\n",
+        			errno, strerror (errno));
+        			exit (1);
+        		}
+
         	printf("Send successful\n");
-	//}
 
-        sleep(2);
+        	sleep(2);
 
-        printf ("Waiting to Receiving on mqd_t %d.\n", rtos);
+        	printf ("Waiting to Receiving on mqd_t %d.\n", rtos);
 
-        if(mq_receive (rtos, buffer, sizeof (buffer), NULL))
-        	printf("Hello I am here\n");
+        	if(mq_receive (rtos, buffer, sizeof (buffer), NULL))
+        		printf("Notiffication successfully sent going back to checking again\n");
 
-        else{
-        	printf ("Error %d (%s) on mq_receive.\n",
-        	errno, strerror (errno));
-        	exit (1);
-        }
-        printf ("Received [%s].\n", buffer);
+        	else{
+        		printf ("Error %d (%s) on mq_receive.\n",
+        		errno, strerror (errno));
+        		exit (1);
+        	}
+//        	printf ("Received [%s].\n", buffer);
 
-        mq_close (rtos);
+        	mq_close (rtos);
+//	}
 
         return 0;
 }
